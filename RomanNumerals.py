415a1895931947e05a6861b786d869e86da62c77
Maggie B <31546455+mBortell88@users.noreply.github.com>
2017-09-07 11:46:10 -0500

    Add files via upload

---
+++ RomanNumerals.py
@@ -0,0 +1,27 @@
+# This program displays Roman Numerals for numbers 1-10
+# Remember to format input to int
+
+number = int(input("Enter a number 1 through 10: "))
+
+if number == 1:
+    print("I")
+elif number == 2:
+    print("II")
+elif number == 3:
+    print("III")
+elif number == 4:
+    print("IV")
+elif number == 5:
+    print("V")
+elif number == 6:
+    print("VI")
+elif number == 7:
+    print("VII")
+elif number == 8:
+    print("VIII")
+elif number == 9:
+    print("IX")
+elif number == 10:
+    print("X")
+else:
+    print("Error! Number entered must be 1 through 10.")
