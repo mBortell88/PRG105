7e0d594d5505dab4b244edfe476e88172d4882ef
Maggie B <31546455+mBortell88@users.noreply.github.com>
2017-09-19 15:32:02 -0500

    Add files via upload

--- PenniesPay.py
+++ PenniesPay.py
@@ -9,12 +9,13 @@ print('Days\t Pennies')
 print('-----------------------------')
 # start accumulator for total
 total = 0.00
+salary = 0.01
 for days in range(1, end+1):
-    salary = 0.01
     # equation will reflect 2 to the power of the day's number
-    pennies_earned = format((2**days)*salary, ',.2f')
-    print(days, '\t', '\t', pennies_earned)
+    pennies_earned = salary
+    print(days, '\t', '\t', '$', format(pennies_earned, ",.2f"))
     # duplicated tab to make table align
     total += float(pennies_earned)
+    salary = salary * 2
 # format the total so it rounds two decimals
-print('Total earned: ', format(total, ',.2f'))
+print('Total earned: ', '$', format(total, ',.2f'))
