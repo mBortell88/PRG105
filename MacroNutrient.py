67b8299a2fc1e908f9ed874f63392d53f17bc47f
Maggie B <31546455+mBortell88@users.noreply.github.com>
2017-09-29 09:23:31 -0500

    Add files via upload

--- MacroNutrient.py
+++ MacroNutrient.py
@@ -3,44 +3,44 @@
 
 
 # global variable, accumulator - totals the calories
-total = 0.00
+total_calories = 0.00
 
 
-def total_calories():
+def main():
     # This function totals all calories from fat, carbs and protein
     # Accesses additional functions
     print("This program calculates the total fat, carbs and protein consumed in a day.")
     fat()
     carb()
     protein()
-    print("The day's total calories: ", total)
+    print("The day's total calories: ", format(total_calories, ",.2f"))
 
 
 def fat():
     # This function asks the user for the amount of fat calories consumed
+    global total_calories
     fat_cal = float(input("How many calories of fat (in grams) were consumed?"))
     total_fat = fat_cal * 9
     print("The day's total calories from fat: ", total_fat)
-    global total
-    total += total_fat
+    total_calories += total_fat
 
 
 def carb():
     # This function asks the user for the amount of carb calories consumed
+    global total_calories
     carb_cal = float(input("How many calories of carbs (in grams) were consumed?"))
     total_carb = carb_cal * 4
     print("The day's total calories from carbs: ", total_carb)
-    global total
-    total += total_carb
+    total_calories += total_carb
 
 
 def protein():
     # This function asks the user for the amount of protein calories consumed
+    global total_calories
     protein_cal = float(input("How many calories of proteins (in grams) were consumed?"))
     total_protein = protein_cal * 4
     print("The day's total calories from protein: ", total_protein)
-    global total
-    total += total_protein
+    total_calories += total_protein
 
 
-total_calories()
+main()
